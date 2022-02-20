fc3d9e638dfe591955ac537ea885fa278eee8266
caoxu <caoxu@DESKTOP-O6CFKHS>
2022-02-20 12:40:07 +0700

    ChainOfRespon partern

---
+++ Partern_ChainOfResponsibility/src/LeaveRequest.java
@@ -0,0 +1,12 @@
+
+public class LeaveRequest {
+    private int days;
+ 
+    public LeaveRequest(int days) {
+        this.days = days;
+    }
+ 
+    public int getDays() {
+        return days;
+    }
+}
