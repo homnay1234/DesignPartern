43d922dc42a4044306cbb1d76683119b69b59106
caoxu <caoxu@DESKTOP-O6CFKHS>
2022-02-20 12:40:59 +0700

    command partern

---
+++ Partern_Command/src/Client.java
@@ -0,0 +1,14 @@
+
+public class Client {
+ 
+    public static void main(String[] args) {
+        Account account = new Account("gpcoder");
+ 
+        Command open = new OpenAccount(account);
+        Command close = new CloseAccount(account);
+        BankApp bankApp = new BankApp(open, close);
+ 
+        bankApp.clickOpenAccount();
+        bankApp.clickCloseAccount();
+    }
+}
---
+++ Partern_Command/src/OpenAccount.java
@@ -0,0 +1,14 @@
+
+public class OpenAccount implements Command {
+ 
+    private Account account;
+ 
+    public OpenAccount(Account account) {
+        this.account = account;
+    }
+ 
+    @Override
+    public void execute() {
+        account.open();
+    }
+}
