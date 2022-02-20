9e68a85aded29fc499a160ced24bdf8c822780a1
caoxu <caoxu@DESKTOP-O6CFKHS>
2022-02-20 12:49:10 +0700

    Strategy Partern

---
+++ Partern_Strategy2/src/MergeSort.java
@@ -0,0 +1,12 @@
+
+import java.util.List;
+ 
+import java.util.List;
+
+public class MergeSort implements SortStrategy {
+ 
+    @Override
+    public <T> void sort(List<T> items) {
+        System.out.println("Merge sort");
+    }
+}
